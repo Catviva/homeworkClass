8d3073c547a558404adfc9ebbab73dd5da395ab1
ekaterinasarapova <nkate05@mail.ru>
2022-05-21 22:30:36 +0300

    Done last hw

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,32 @@
+import java.util.Objects;
+
 public class Book {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishingYear == book.publishingYear && title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publishingYear);
+    }
 
     private String title;
     private Author author;
     private int publishingYear;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", publishingYear=" + publishingYear +
+                '}';
+    }
 
     public Book(String title, Author author, int publishingYear) {
         this.title = title;
